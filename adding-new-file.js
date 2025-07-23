98e2c2bb708e64563e043ba18a6a35f14290f5be
Mudit01-23 <muditjoshi0305@gmail.com>
2025-07-23 16:25:00 +0530

    updated adding-new-file.js

--- adding-new-file.js
+++ adding-new-file.js
@@ -1,1 +1,3 @@
 console.log("I've cloned this repo from github to the GitHub folder on my desktop, and then making changes I've cloned this repo from github to the GitHub folder on my desktop, and then making changes in itI've cloned this repo from github to the GitHub folder on my desktop, and then making changes in it");
+
+here i've made change inside the new branch- feature
